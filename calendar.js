eb301eb47b80aed0fd9461b382c16de274671cf1
Alex Jimmy <alu0100728170@ull.edu.es>
2017-05-18 02:38:45 +0100

    Creacion Clase Calendario. RemainingDays implementado

---
+++ calendar.js
@@ -0,0 +1,61 @@
+'use strict'
+var NAME_DAYS = ["Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"];
+var MONTHS= [{name: 'Enero', days: 31}, {name: 'Febrero', days: 28}, {name: 'Marzo', days: 31 },
+        {name: 'Abril', days: 30 }, {name: 'Mayo', days: 31 }, {name: 'Junio',days: 30 },
+        {name: 'Julio', days: 31 }, {name: 'Agosto', days: 31 }, {name: 'Septiembre', days: 30 },
+        {name: 'Octubre', days: 31}, {name: 'Noviembre', days: 30 }, {name: 'Diciembre', days: 31 }]
+
+var FULL_CURRENT_DATE = new Date();
+
+var CURRENT_DATE = {
+    day: FULL_CURRENT_DATE.getDate(), // 1-30(~)
+    month: FULL_CURRENT_DATE.getMonth()+1, //0-11
+    year: FULL_CURRENT_DATE.getFullYear(), //2017
+    day_in_week: FULL_CURRENT_DATE.getDay() //0-6
+}
+
+class Calendar {
+    constructor(date) {
+        date = date || CURRENT_DATE;
+
+        date.month--; //Ajustar acceso al array;
+        this.current = Object.assign({}, date);
+        console.log(this.current);
+        this.isLeap = date.year % 4 == 0;
+    }
+    month(m){
+        if(m<0 || m>11) throw 'Mes incorrecto';
+        var month = MONTHS[m];
+        return month;
+    }
+    get toDays(){
+        var date = this.current;
+        var days = date.day;
+        //Suma los dias de los meses
+        for(var i=0; i<date.month; i++){
+            days += this.month(i).days;
+        }
+        console.log(days);
+        //Suma los dias del año
+        days += Math.floor(date.year * 365.25);
+
+        //Sumar dias bisiestos;
+        if( ( (date.month<2 && date.day <=29) ||(date.month <1) ) && ( this.isLeap ) ) {
+            console.log("Descontanto dia bisiesto");
+            days--;
+        }
+
+        return days;
+    }
+}
+
+
+function remainingDays(datesA, dateB){
+    dateB = dateB || CURRENT_DATE;
+    var calB = new Calendar(dateB);
+    for(var i=0; i<datesA.length; i++){
+        var dateA = datesA[i];
+        var calA = new Calendar(dateA);
+        datesA[i].remaining =  calB.toDays -calA.toDays;
+    }
+}
