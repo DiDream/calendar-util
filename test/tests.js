8d5b39fd254db06185c14e2990fe2fc6b5c58c35
Alex Jimmy <alu0100728170@ull.edu.es>
2017-05-19 02:23:19 +0100

    Tests reescritos. Nuevo test para dias siguientes

--- test/tests.js
+++ test/tests.js
@@ -1,19 +1,32 @@
 'use strict'
 let Calendar = require('../calendar');
 
-describe('Calendar Utils',function(){
+describe('Calendar Util remaining days',function(){
     it('10 remaining days respect 20/2/2016 - 1/3/2016',function(){
-        var current_date = new Date();
-
-        var dateA = [{day:20, month:2, year: 2016}]
-        Calendar.remainingDays(dateA, {day:1, month:3, year: 2016});
-        dateA[0].remaining.should.be.equal(10);
+        let calendar = new Calendar({day:20, month:2, year: 2016})
+        let dateEnd =[{day:1, month:3, year: 2016}];
+        calendar.remainingDays(dateEnd);
+        dateEnd[0].remaining.should.be.equal(10);
     });
     it('Not 365 remaining days respect 1/1/2016 - 1/1/2017',function(){
-        var current_date = new Date();
+        let calendar = new Calendar({day:1, month:1, year: 2016})
 
-        var dateA = [{day:1, month:1, year: 2016}]
-        Calendar.remainingDays(dateA, {day:1, month:1, year: 2017});
-        dateA[0].remaining.should.not.be.equal(365);
+        let dateEnd = [{day:1, month:1, year: 2017}]
+        calendar.remainingDays(dateEnd);
+        dateEnd[0].remaining.should.not.be.equal(365);
     });
 });
+describe('Calendar Util next days',function(){
+    it('28,29,1,2 since 28-2-2016 and next 4 days',function(){
+        var expect = [ [28,29],[1,2] ];
+        let calendar = new Calendar({day:28, month: 2, year:2016});
+        let months = calendar.nextDays(4);
+        //console.log(JSON.stringify(months,null,2));
+        for(var i=0; i<months.length; i++){
+            var days = months[i].days;
+            for(var j=0; j<days.length;j++ ){
+                months[i].days[j].date.should.be.equal(expect[i][j]);
+            }
+        }
+    });
+})
